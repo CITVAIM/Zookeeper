81c9ec214580202720df3d4b9cf9720cabb9c5bd
YangYang <3221985983@qq.com>
2023-02-07 18:05:30 +0800

    Initial commit

---
+++ Topics/Scanning the input/Hiring a chef/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
+        // start coding here
+        String name = scanner.next();
+        String years = scanner.next();
+        String like = scanner.next();
+
+
+        System.out.println("The form for " + name + " is completed. We will contact you if we need a chef who cooks " + like + " dishes and has " + years + " years of experience.");*/
+//        System.out.println(((12241 % 100) / 10) % 10);
+        /*int n = scanner.nextInt();
+         n = ((n + 1) * n + 2) * n + 3;
+        System.out.println(n);*/
+
+        /*int n = scanner.nextInt() - 1;
+        int n2 = scanner.nextInt() - 1;
+        int n3 = scanner.nextInt() - 1;
+        int n4 = scanner.nextInt() - 1;
+        System.out.print(n + " ");
+        System.out.print(n2 + " ");
+        System.out.print(n3 + " ");
+        System.out.println(n4);*/
+        /*int result = 5;
+        int a = 1;
+        int b = --a;
+        a++;
+        result += a + (--a / ++b);
+        System.out.println(result);*/
+ /*       char c = '1' + '3';
+        System.out.println(c);*/
+        String str = "abcs2 !$|_-阿啊大大大吖3分3";
+        System.out.println(str.length());
+        System.out.println(str.charAt(15));
+
+    }
+}
---
+++ Topics/String/Comparing strings ignoring whitespaces/src/Main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);
+        // start coding here
+        String str =scanner.nextLine().replaceAll(" ","");
+        String str2 =scanner.nextLine().replaceAll(" ","");
+
+        System.out.println(str.equals(str2));*/
+        String confessionLove = """
+                I
+                love                   
+                """;
+
+        String favoriteLanguage = "Java";
+
+        System.out.println(confessionLove + favoriteLanguage);
+    }
+}
